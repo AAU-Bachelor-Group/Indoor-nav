0e9fba423ddea38ae3271720d3dc934c70543297
Oliver Roed Schøler <oliver@roedscholer.dk>
2026-03-22 15:19:33 +0100

    Update deps

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,12 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite"
 import viteReact from "@vitejs/plugin-react"
 import tailwindcss from "@tailwindcss/vite"
 import { nitro } from "nitro/vite"
-import tsconfigPaths from "vite-tsconfig-paths"
 
 const config = defineConfig({
+  resolve: {
+    tsconfigPaths: true,
+  },
   plugins: [
-    tsconfigPaths(),
     devtools(),
     nitro({ rollupConfig: { external: [/^@sentry\//] } }),
     tailwindcss(),
