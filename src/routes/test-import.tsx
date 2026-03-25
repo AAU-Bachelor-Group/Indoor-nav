e6e74065601b40f4fe7f0891c732fed7a942386b
Mikkel Brun Larsen <mikkelbrunl@gmail.com>
2026-03-25 10:56:23 +0100

    added buttons for convenience

--- src/routes/test-import.tsx
+++ src/routes/test-import.tsx
@@ -1,9 +1,22 @@
-import { createFileRoute } from "@tanstack/react-router"
+import { createFileRoute, Link } from "@tanstack/react-router"
+
+import { buttonVariants } from "#/components/ui/button"
 
 import ImportFloorForm from "../components/import-floor/import-floor-form"
 
 const RouteComponent = () => {
-  return <ImportFloorForm />
+  return (
+    <div>
+      {/* for convinence again */}
+      <div className="flex justify-center">
+        <Link to="/" className={buttonVariants({ variant: "default" })}>
+          Go back
+        </Link>
+      </div>
+
+      <ImportFloorForm />
+    </div>
+  )
 }
 
 export const Route = createFileRoute("/test-import")({
