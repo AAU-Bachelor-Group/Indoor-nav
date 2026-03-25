99a88c43d4c431a524110f290c68f96227393080
Mikkel Brun Larsen <mikkelbrunl@gmail.com>
2026-03-25 10:30:10 +0100

    Revert "added buttons for convinence"
    This reverts commit 89420526861b6bab84102298df897588845f6385.

--- src/routes/test-import.tsx
+++ src/routes/test-import.tsx
@@ -1,21 +1,9 @@
-import { createFileRoute, Link } from "@tanstack/react-router"
+import { createFileRoute } from "@tanstack/react-router"
 
 import ImportFloorForm from "../components/import-floor/import-floor-form"
-import { buttonVariants } from "#/components/ui/button"
 
 const RouteComponent = () => {
-  return (
-    <div>
-      { /* for convinence again */}
-      <div className="flex justify-center">
-        <Link to="/" className={buttonVariants({ variant: "default" })}>
-          Go back
-        </Link>
-      </div>
-
-      <ImportFloorForm />
-    </div>
-  )
+  return <ImportFloorForm />
 }
 
 export const Route = createFileRoute("/test-import")({
