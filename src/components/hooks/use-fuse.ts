1d9f59893f5cc2229f2f0199cb1a2f2dc38e6c63
Mikkel Brun Larsen <mikkelbrunl@gmail.com>
2026-04-07 08:32:23 +0200

    added weights

--- src/components/hooks/use-fuse.ts
+++ src/components/hooks/use-fuse.ts
@@ -1,18 +1,26 @@
-import { useQuery } from '@tanstack/react-query'
-import { getAllRoomsFunction } from '#/server/search.functions'
-import Fuse from 'fuse.js'
+import { useQuery } from "@tanstack/react-query"
+import { getAllRoomsFunction } from "#/server/search.functions"
+import Fuse from "fuse.js"
 
 export const useFuzzySearch = (searchTerm: string) => {
-  const { data: allRooms, isLoading, isError } = useQuery({
+  const {
+    data: allRooms,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ["getAllRooms"],
     queryFn: getAllRoomsFunction,
   })
 
   const fuse = new Fuse(allRooms ?? [], {
-    keys: ['id', 'title', 'semanticNames', 'type'],
+    keys: [
+      { name: "id", weight: 0.7 },
+      { name: "semanticNames", weight: 0.3 },
+    ],
+    threshold: 0.4,
   })
 
   const results = fuse.search(searchTerm)
-  
+
   return { results, isLoading, isError }
 }
